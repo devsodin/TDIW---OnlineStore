47a9ff236a86c3b170c85013808cde87848b2f8e
Devsodin <devsodin@gmail.com>
2017-11-08 20:16:09 +0100

    tempostorm

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+    <hmtl>
+        <head>
+            <title>ENP</title>
+            <meta lang="en" charset="UTF-8">
+            <link rel="icon" href="a.ico" type="image/x-icon"/>
+            <link rel=stylesheet href="view/default/css/index.css"/>
+        </head>
+        <body>
+            <nav>
+                <a href="index.php"><img alt="Title" src="images/logo.png"></a>
+
+                <ul class=""></ul>
+            </nav>
+        </body>
+    </hmtl>
